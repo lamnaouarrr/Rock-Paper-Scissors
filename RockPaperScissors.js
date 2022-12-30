aece3b713a72afda63c6445fa6ab12163ff4533a
Ayoub <ayoublamnaouar1@gmail.com>
2022-12-30 23:27:41 +0100

    .

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,42 +1,73 @@
 let computerScore = 0;
 let playerScore = 0;
 let computerChoice = ["rock", "paper", "scissors"];
+let playerSelection;
+let rounds = 0;
+const container = document.querySelector('#container');
+const content = document.createElement('div');
+let computerSelection;
 
 function getComputerChoice() {
     return computerChoice[Math.floor(Math.random() * computerChoice.length)];
 }
 
+function checkRound(){
+    if (computerScore == playerScore){
+        content.textContent = "Draw!\nYour score is: "+playerScore+"\nComputer's score is: "+computerScore;
+    }
+    else if(computerScore > playerScore){
+        content.textContent = "Unlucky,you lose.\nYour score is: "+playerScore+"\nComputer's score is: "+computerScore;
+    }
+    else{
+        content.textContent = "Congratulations, you win!\nYour score is: "+playerScore+"\nComputer's score is: "+computerScore;
+    }
+}
+
+function initialize(){
+    computerScore = 0;
+    playerScore = 0;
+    rounds = 0;
+}
+
 
 function playRound(playerSelection, computerSelection) {
+    console.log(playerSelection, computerSelection);
     if (playerSelection == computerSelection){
-        return "Draw! you got no point";
+        content.textContent = "Draw! you got no point";
     }
     else if(playerSelection == "rock" && computerSelection == "scissors" || playerSelection == "scissors" && computerSelection == "paper" || playerSelection == "paper" && computerSelection == "rock"){
-        playerScore =+1;
-        return "One point for you! "+playerSelection+" beats "+computerSelection+".";
+        playerScore++;
+        content.textContent = "One point for you! "+playerSelection+" beats "+computerSelection+".";
     }
     else{
-        computerScore =+1;
-        return "The computer got one point! "+computerSelection+" beats "+playerSelection+".";
+        computerScore++;
+        content.textContent = "The computer got one point! "+computerSelection+" beats "+playerSelection+".";
     }
+    if(rounds == 4){
+        checkRound();
+        initialize();
+    }
+    container.appendChild(content);
 }
 
-function game() {
-    for (let i = 1; i <= 5; i++){
-        const computerSelection = getComputerChoice();
-        const playerSelection = prompt("What is your weapon? Rock, Paper or Scissors?");
-        alert(playRound(playerSelection.toLowerCase(), computerSelection));
-    }
-    if (computerScore == playerScore){
-        alert("Draw!\nYour score is: "+playerScore+"\nComputer's score is: "+computerScore);
-    }
-    else if(computer > player){
-        alert("Unlucky,you lose.\nYour score is: "+playerScore+"\nComputer's score is: "+computerScore);
-    }
-    else{
-        alert("Congratulations, you win!\nYour score is: "+playerScore+"\nComputer's score is: "+computerScore);
-    }
-}
+document.getElementById('button1').addEventListener('click', function() {
+    computerSelection = getComputerChoice();
+    playerSelection = "rock";
+    playRound(playerSelection, computerSelection);
+    rounds++;
+});
+document.getElementById('button2').addEventListener('click', function() {
+    computerSelection = getComputerChoice();
+    playerSelection = "paper";
+    playRound(playerSelection, computerSelection);
+    rounds++;
+});
+document.getElementById('button3').addEventListener('click', function() {
+    computerSelection = getComputerChoice();
+    playerSelection = "scissors";
+    playRound(playerSelection, computerSelection);
+    rounds++;
+});
 
-game();
-
+content.style.color = 'black';
+container.appendChild(content);
